39e2ea0c17ddb2c4ff05e6036a81287de48e7026
Archie Healy <archiethehead@gmail.com>
2026-05-24 20:24:50 +0100

    main.html
    Feat: Implemented a HTML content server from the index.js file, sending the main web page as a HTTP response.
    
    Chore: Implemented a run.bat file, which launches a webpage instance, and the node server.

--- Public/JS/index.js
+++ Public/JS/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 // Route handling
 app.get('/', (req, res) => {
-    res.send('<h2>Hello from Express.js server!!</h2>');
+
+    res.sendFile(path.join(__dirname, '../HTML/main.html'));
+
+
 });
 
 // Server setup
 app.listen(8080, () => {
+
     console.log('server listening on port 8080');
+    
 });
